146a9dd2c990f456cc517e33cbf54b3ceca416e3
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-01 10:59:06 +0300

    GraphVis With Dijkstra And Shortcuts. Js alert corrected.

--- Graph Visualization/sketch.js
+++ Graph Visualization/sketch.js
@@ -10,9 +10,12 @@ const backgroundColor = 80;
 function setup() {
   createCanvas(1200, 800);
   textSize(txtSize);
-  // alert("You can use 'Enter' and 'Delete' to create or delete a node at mouse position!" +
-  // " Additionally, use the 'Shift' key to create an edge between nodes!"+ " For that first hover over on a node and press 'Shift'."
-  //     +" Then, hover over the want-to-connect node then press it again." + " This will take the entered data from the input box 'Weight'" +
+  // alert("You can use 'Enter' and 'Delete' to create" +
+  // "or delete a node at mouse position!" +
+  // " Additionally, use the 'Shift' key to create an edge between nodes!"+
+  // " For that first hover over on a node and press 'Shift'."+
+  // " Then, hover over the want-to-connect node then press it again." +
+  // " This will take the entered data from the input box 'Weight'" +
   //      " and create and edge for you." + " Cha cha real smooth!");
   createDomElements();
 }
