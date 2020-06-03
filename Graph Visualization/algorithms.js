c85eff603bb905ba3a7d5d09f7a22f63db71fc66
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-03 23:24:15 +0300

    added unworking prims

--- Graph Visualization/algorithms.js
+++ Graph Visualization/algorithms.js
@@ -1,14 +1,14 @@
-function dijkstraForAllNodes(globalArr,key,retObj) {
+function dijkstraForAllNodes(globalArr, key, retObj) {
   let Q = [];
   let nodes = [...globalArr];
 
   for (let node of nodes) {
     node.distance = Infinity;
     node.prev = NaN;
-   Q.push(node);
+    Q.push(node);
   }
 
-  let keyIdx = nodeIdxFinder(Q,key);
+  let keyIdx = nodeIdxFinder(Q, key);
   Q[keyIdx].distance = 0;
   Q[keyIdx].prev = Q[keyIdx];
 
@@ -16,69 +16,98 @@ function dijkstraForAllNodes(globalArr,key,retObj) {
     let txtDist = "";
     let u = minElement(Q);
     let connectionsArr = u.connections;
-    findRemove(Q,u);
+    findRemove(Q, u);
     for (let connection of connectionsArr) {
-        let v = connection.node;
+      let v = connection.node;
 
-        let alt = u.distance + connection.weight;
-        if (alt < v.distance) {
-          v.distance = alt;
-          v.prev = u;
-        }
+      let alt = u.distance + connection.weight;
+      if (alt < v.distance) {
+        v.distance = alt;
+        v.prev = u;
+      }
     }
-    for(let node of nodes)
+    for (let node of nodes)
       txtDist += node.distance.toString() + " ";
-    retObj.table = retObj.table.concat("Node " + u.data +" Distances: " + txtDist + '<br>');
+    retObj.table = retObj.table.concat("Node " + u.data + " Distances: " + txtDist + '<br>');
   }
-  for(let node of nodes)
+  for (let node of nodes)
     retObj.path = retObj.path.concat("Node " + node.data + "," + node.prev.data + " ");
 }
 
-function minElement(nodeArr){
+function minElement(nodeArr) {
   let retNode = nodeArr[0];
-  for(let node of nodeArr){
-    if(node.distance<retNode.distance){
+  for (let node of nodeArr) {
+    if (node.distance < retNode.distance) {
       retNode = node;
     }
   }
   return retNode;
 }
 
-function findRemove(nodeArr,node){
-  for (let i=0; i<nodeArr.length;i++){
-    if(nodeArr[i]===node){
-      nodeArr.splice(i,1);
+function findRemove(nodeArr, node) {
+  for (let i = 0; i < nodeArr.length; i++) {
+    if (nodeArr[i] === node) {
+      nodeArr.splice(i, 1);
       return;
     }
   }
 }
 
-function prims_mst(globalArr,retObj){
+// function kruskal_mst(globalArr) {
+//   // 1. Sort all the edges in non-decreasing order of their weight.
+//   // 2. Pick the smallest edge. Check if it forms a cycle with the spanning tree formed so far.
+//   // If cycle is not formed, include this edge. Else, discard it.
+//   // 3. Repeat step#2 until there are (V-1) edges in the spanning tree.
+//
+//   let edges = [];
+//   for (let node of globalArr) {
+//     for (let connection of node.connections) {
+//       edges.push();
+//     }
+//   }
+//   edges = edges.sort((a, b) => {
+//     if (a.weight > b.weight)
+//       return 1;
+//     else
+//       retu
+//   });
+//
+//   paintEdges(mst, pink);
+// }
+
+
+
+function prims_mst(globalArr) {
   // check geeks by geeks for additional Info;
   //src = https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/;
-  // 1) Create a set mstSet that keeps track of vertices already included in MST.
-  // 2) Assign a key value to all vertices in the input graph.
-  // Initialize all key values as INFINITE.
-  // Assign key value as 0 for the first vertex so that it is picked first.
-  // 3) While mstSet doesn’t include all vertices
-  // ….a) Pick a vertex u which is not there in mstSet and has minimum key value.
-  // ….b) Include u to mstSet.
-  // ….c) Update key value of all adjacent vertices of u.
-  //  To update the key values, iterate through all adjacent vertices.
-  //  For every adjacent of v, if weight of edge u-v is less than the previous key value of v,
-  //   update the key value as weight of u-v
+  let mst = [];
+  let nodes = [...globalArr];
+  //As our node class already has an attribute called distance
+  //We will use it instead of creating a new attribute called key
+  for (let node of nodes) {
+    node.distance = Infinity;
+  }
+  nodes[0].distance = 0;
 
-  // let mst = [];
-  // let nodes = [...globalArr];
-  // //As our node class already has an attribute called distance we will use it
-  // //instead of creating a new attribute called key
-  // for(let node of nodes){
-  //   node.distance = Infinity;
-  // }
-  // nodes[0].distance = 0;
-  //
-  // while(mst.length!=nodes.length){
-  //   let u = minElement(Q);
-  //   findRemove(Q,u);
-  // }
+  while (mst.length != nodes.length) {
+    let u = minElement(nodes);
+    if (mst.includes(u)) {
+      nodes.splice(nodeIdxFinder(nodes, u.data), 1);
+      continue;
+    }
+    mst.push(u);
+    for (connection of u.connections) {
+      if (connection.weight < connection.node.distance)
+        connection.distance = connection.weight;
+    }
+  }
+  paintEdges(mst, pink);
+}
+
+function paintEdges(nodeArr, color) {
+  for (let node of nodeArr) {
+    for (let connection of node.connections) {
+      connection.colorStr = color;
+    }
+  }
 }
