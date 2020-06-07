7c99b63fccde178ef06e620f4907df0dc1a85cf3
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-08 00:12:47 +0300

    Isolated i/o stuff into data_io

--- Graph Visualization/buttons_and_actions.js
+++ Graph Visualization/buttons_and_actions.js
@@ -128,42 +128,10 @@ function createDataIoButtons() {
   dataDownloadButton = createButton("Download Graph");
   dataDownloadButton.position(deleteNodeButton.x + 430, deleteNodeButton.y);
   dataDownloadButton.mousePressed(() => {
-    saveJSON(nodes, 'graph.json');
+    saveJsonWithCircular();
   });
 }
 
-function jsonToObject(objArr) {
-  //first revive all the nodes then we can continue with connections array
-  for (let i in objArr) {
-    let newNode = new Node();
-    let currObj = objArr[i];
-    newNode.x = currObj.x;
-    newNode.y = currObj.y;
-    newNode.data = currObj.data;
-    newNode.r = currObj.r;
-    newNode.distance = currObj.distance;
-    newNode.prev = currObj.prev;
-    nodes.push(newNode);
-    addNewSelections(newNode);
-    nodeIndexIter++;
-  }
-  //now the connections
-  for (let i in objArr) {
-    let currObj = objArr[i];
-    let currNode = nodes[nodeIdxFinder(nodes, currObj.data)];
-    for (let j in currObj.connections) {
-      if (!currObj.connections[j])
-        continue;
-      let newConnection = new Connection();
-      let jsonConnection = currObj.connections[j];
-      let nodePointer = nodes[nodeIdxFinder(nodes, jsonConnection.node.data)];
-      newConnection.node = nodePointer;
-      newConnection.weight = jsonConnection.weight;
-      newConnection.colorStr = jsonConnection.colorStr;
-      currNode.connections.push(newConnection);
-    }
-  }
-}
 
 //Node Actions
 function newNodeOnPress(coordX, coordY) {
