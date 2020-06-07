c1bd948a2ae1aac68b2265c6469941fe7bb88517
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-07 23:13:50 +0300

    Working Graph I/O

--- Graph Visualization/buttons_and_actions.js
+++ Graph Visualization/buttons_and_actions.js
@@ -98,6 +98,7 @@ function createAlgoButtons() {
   // kruskalButton.position(dijkstraButton.x, dijkstraButton.y+30);
   // kruskalButton.mousePressed(() => kruskal_mst(nodes));
 }
+
 function createTableAndPath(posObject, txtPath, txtTable, retObj) {
   txtPath.html("Path: " + '<br>' + retObj.path);
   txtPath.position(posObject.x, posObject.y + 30);
@@ -130,19 +131,37 @@ function createDataIoButtons() {
     saveJSON(nodes, 'graph.json');
   });
 }
-function jsonToObject(obj){
-  for (x in obj) {
-    node = new Node();
-    node.x = obj[x].x;
-    node.y = obj[x].y;
-    node.data = obj[x].data;
-    node.r = obj[x].r;
-    node.connections = obj[x].connections;
-    node.distance = obj[x].distance;
-    node.prev = obj[x].prev;
-    nodes.push(node);
-    addNewSelections(node);
+
+function jsonToObject(objArr) {
+  //first revive all the nodes then we can continue with connections array
+  for (let i in objArr) {
+    let newNode = new Node();
+    let currObj = objArr[i];
+    newNode.x = currObj.x;
+    newNode.y = currObj.y;
+    newNode.data = currObj.data;
+    newNode.r = currObj.r;
+    newNode.distance = currObj.distance;
+    newNode.prev = currObj.prev;
+    nodes.push(newNode);
+    addNewSelections(newNode);
     nodeIndexIter++;
+  }
+  //now the connections
+  for (let i in objArr) {
+    let currObj = objArr[i];
+    let currNode = nodes[nodeIdxFinder(nodes, currObj.data)];
+    for (let j in currObj.connections) {
+      if (!currObj.connections[j])
+        continue;
+      let newConnection = new Connection();
+      let jsonConnection = currObj.connections[j];
+      let nodePointer = nodes[nodeIdxFinder(nodes, jsonConnection.node.data)];
+      newConnection.node = nodePointer;
+      newConnection.weight = jsonConnection.weight;
+      newConnection.colorStr = jsonConnection.colorStr;
+      currNode.connections.push(newConnection);
+    }
   }
 }
 
