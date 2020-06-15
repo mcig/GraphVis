3f912492157cf729a852422fdc581afd96bb6027
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-15 10:54:40 +0300

    Activated the welcome alert.

--- public/sketch.js
+++ public/sketch.js
@@ -10,13 +10,13 @@ const backgroundColor = 80;
 function setup() {
   createCanvas(1200, 800);
   textSize(txtSize);
-  // alert("You can use 'Enter' and 'Delete' to create" +
-  // "or delete a node at mouse position!" +
-  // " Additionally, use the 'Shift' key to create an edge between nodes!"+
-  // " For that first hover over on a node and press 'Shift'."+
-  // " Then, hover over the want-to-connect node then press it again." +
-  // " This will take the entered data from the input box 'Weight'" +
-  // " and create and edge for you." + " Cha cha real smooth!");
+  alert("Welcome! You can use 'Enter' and 'Delete' to create" +
+  "or delete a node at mouse position!" +
+  " Additionally, use the 'Shift' key to create an edge between nodes!"+
+  " For that first hover over on a node and press 'Shift'."+
+  " Then, hover over the want-to-connect node then press it again." +
+  " This will take the entered data from the input box 'Weight'" +
+  " and create and edge for you." + " Cha cha real smooth!");
   createDomElements();
 }
 
