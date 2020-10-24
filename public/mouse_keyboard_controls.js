9cf96a3b9ce6626897d13fe32620c4753df9a20b
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-10-24 13:58:57 +0300

    fixed buggy mouse drag

--- public/mouse_keyboard_controls.js
+++ public/mouse_keyboard_controls.js
@@ -1,47 +1,61 @@
 let detectedDims = [];
+let lockedOnNode = false;
+let lockedNode = NaN;
+
+function mousePressed() {
+  if ((lockedNode = findNodeByCoord(mouseX, mouseY))) {
+    lockedOnNode = true;
+  } else {
+    lockedOnNode = false;
+  }
+}
 
 function mouseDragged() {
   //reverse search is used to hold the top-most ball first
-  let curr = findNodeByCoord(mouseX, mouseY);
-  if (curr) {
-    curr.x = mouseX;
-    curr.y = mouseY;
+  if (lockedOnNode) {
+    lockedNode.x = mouseX;
+    lockedNode.y = mouseY;
   }
 }
 
 function doubleClicked() {
   let curr = findNodeByCoord(mouseX, mouseY);
-  if (!curr)
-    return;
-  if (dataInput.value())
-    curr.data = dataInput.value();
+  if (!curr) return;
+  if (dataInput.value()) curr.data = dataInput.value();
 }
 
 function keyPressed() {
   //// TODO: solve the first click is missed bug
 
-  if (mouseX > width || mouseX < 0 || mouseY > height || mouseY < 0)
-    return;
+  if (mouseX > width || mouseX < 0 || mouseY > height || mouseY < 0) return;
 
   if (keyCode === SHIFT) {
     detectedDims.push(mouseX);
     detectedDims.push(mouseY);
     //basicaly we store the last 2 if they are valid
     //and if they are all valid ve draw a connection
-    if (detectedDims.length >= 4 && findNodeByCoord(detectedDims[0], detectedDims[1]) && //this is just spaghetti
-      findNodeByCoord(detectedDims[2], detectedDims[3])) {
-      let fromIdx = nodeIdxFinder(nodes, findNodeByCoord(detectedDims[0], detectedDims[1]).data);
-      let toIdx = nodeIdxFinder(nodes, findNodeByCoord(detectedDims[2], detectedDims[3]).data);
+    if (
+      detectedDims.length >= 4 &&
+      findNodeByCoord(detectedDims[0], detectedDims[1]) && //this is just spaghetti
+      findNodeByCoord(detectedDims[2], detectedDims[3])
+    ) {
+      let fromIdx = nodeIdxFinder(
+        nodes,
+        findNodeByCoord(detectedDims[0], detectedDims[1]).data
+      );
+      let toIdx = nodeIdxFinder(
+        nodes,
+        findNodeByCoord(detectedDims[2], detectedDims[3]).data
+      );
       let weightVal = int(newEdgeWeightInp.value());
-      if (!weightVal)
-        weightVal = 0;
+      if (!weightVal) weightVal = 0;
       nodes[fromIdx].connect(nodes[toIdx], weightVal);
       for (let i = 0; i < 4; i++) {
         detectedDims.pop();
       }
-    } else if (detectedDims.length >= 4) { //the second click is missed
-      for (let i = 0; i < 4; i++)
-        detectedDims.splice(2, 2);
+    } else if (detectedDims.length >= 4) {
+      //the second click is missed
+      for (let i = 0; i < 4; i++) detectedDims.splice(2, 2);
     }
   } else if (keyCode === DELETE && findNodeByCoord(mouseX, mouseY)) {
     let value = findNodeByCoord(mouseX, mouseY).data;
@@ -49,18 +63,16 @@ function keyPressed() {
     deleteNodeOnPress(idx, value.toString());
   } else if (keyCode === ENTER) {
     newNodeOnPress(mouseX, mouseY);
-  }else if(keyCode === 17){
+  } else if (keyCode === 17) {
     startX = mouseX;
     startY = mouseY;
   }
 }
 
-
-
 function findNodeByCoord(coordX, coordY) {
   for (let i = nodes.length - 1; i >= 0; i--) {
     let curr = nodes[i];
-    if (dist(coordX, coordY, curr.x, curr.y) < (curr.r + 5)) {
+    if (dist(coordX, coordY, curr.x, curr.y) < curr.r + 5) {
       return curr;
     }
   }
